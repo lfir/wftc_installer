c29caf849c1df7f8760bf810cef96b1c3788ee19
Asta666 <ltee885@gmail.com>
2016-09-03 00:54:33 -0300

    working cp & bk on win

--- wftcInstaller/AuxMeth.java
+++ wftcInstaller/AuxMeth.java
@@ -1,60 +1,136 @@
 package wftcInstaller;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class AuxMeth
 {
-    public static void copyFolder(File sourceFolder, File destinationFolder) throws IOException
-    {
-    	//Recursively copies a directory and its contents overwriting existing ones
+	
+	//actions
+    public static void copyFolder(File sourceFolder, File destinationFolder) throws IOException {
+    	//Recursively copies a directory and its contents overwriting existing files
+    	FileSystem fs = FileSystems.getDefault();
+    	final Path tmp = fs.getPath(destinationFolder.getParent());
         //Check if sourceFolder is a directory or file
-        //If sourceFolder is file; then copy the file directly to new location
-        if (sourceFolder.isDirectory()) 
-        {
-            //Verify if destinationFolder is already present; If not then create it
-            if (!destinationFolder.exists()) 
-            {
-                destinationFolder.mkdir();
-                //System.out.println("Directory created :: " + destinationFolder);
-            }
+        if (sourceFolder.isDirectory()) {
+            //Verify if destinationFolder is already present; if not then create it
+        	mkDirIfNotPresent(destinationFolder);
              
             //Get all files from source directory
             String files[] = sourceFolder.list();
              
             //Iterate over all files and copy them to destinationFolder one by one
-            for (String file : files) 
-            {
+            for (String file : files) {
                 File srcFile = new File(sourceFolder, file);
                 File destFile = new File(destinationFolder, file);
-                 
-                //Recursive function call
-                copyFolder(srcFile, destFile);
+                copyFolder(srcFile, destFile); //Recursive function call
             }
+        } else {
+            //If sourceFolder is file; then copy the file directly to new location
+        	copyFile(sourceFolder.toPath(), destinationFolder.toPath(), tmp);
         }
-        else
-        {
-            //Copy the file content from one place to another 
-        	Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+    
+    public static void copyFile(Path sourceFile, Path destinationFile, Path parentFolder) throws IOException {
+    	if (parentFolder != null) { // null will be returned if the path has no parent
+    	    Files.createDirectories(parentFolder); 
+        	Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
             //System.out.println("File copied :: " + destinationFolder);
+    	}
+	}
+
+	public static void mkDirIfNotPresent(File folder) {
+        if (!folder.exists()) {
+        	folder.mkdir();
+            //System.out.println("Directory created :: " + destinationFolder);
         }
     }
+	
+	public static void bkFilesIfExist(String localDir, File resFolder, File destinationFolder) throws IOException {
+		String q1 = "";
+		Scanner scanFile = new Scanner(new File(resFolder + "/wftc-modified-file-list.txt"));
+		while (scanFile.hasNextLine()) {
+			File nf = new File(localDir + q1);
+			if(nf.exists()) {
+				Path df = destinationFolder.toPath();
+				Path np = nf.toPath();
+				Files.copy(np, df.resolve(np.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+			
+			}
+			q1 = scanFile.nextLine();
+		}
+		scanFile.close();
+	}
+	
+	public static void renameDir(File origDir, String desName) throws IOException {
+	//rename non empty dir too
+		Files.move(origDir.toPath(), (new File(desName).toPath()), StandardCopyOption.ATOMIC_MOVE);
+	}
     
-    public static void runUnpacker(String path, String os) throws IOException {
-    	//Run m2tw unpacker for americas mod
-    	List<String> cmdAndArgs = Arrays.asList("cmd", "/c", "unpack_americas.bat");
-    	if(os == "unix") {
-    		cmdAndArgs.add(0, "wine");
-    	}
-    	File dir = new File(path + "/tools/unpacker");
-    	ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
-    	pb.directory(dir);
-    	Process p = pb.start();	
+    public static void executeCommand(List<String> cmdAndArgs, String dirPath) throws IOException, InterruptedException {
+	    	File dir = new File(dirPath);
+	    	ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+	    	pb.directory(dir);
+	    	Process p = pb.start();
+	    	p.waitFor();
+    }
+        
+    public static void executeShCommands(List<String> commands) throws IOException, InterruptedException {
+        File tempScript = createTempScript(commands);
+        try {
+            ProcessBuilder pb = new ProcessBuilder("bash", tempScript.toString());
+            pb.inheritIO();
+            Process process = pb.start();
+            process.waitFor();
+        } finally {
+            tempScript.delete();
+        }
+    }
+        
+    public static File createTempScript(List<String> commands) throws IOException {
+        File tempScript = File.createTempFile("script", null);
+        Writer streamWriter = new OutputStreamWriter(new FileOutputStream(tempScript));
+        PrintWriter printWriter = new PrintWriter(streamWriter);
+        printWriter.println("#!/bin/bash");
+        for(String command : commands) {
+        	printWriter.println(command);
+        }
+        printWriter.close();
+        return tempScript;
     }
     
+    public static int showTextMessage()	{
+		return JOptionPane.showConfirmDialog(null,
+				"War for the Colonies installer.\n" +
+				"Copyright (C) 2016 Asta666.\n" +
+				"This program is free software: you can redistribute it and/or modify it under the terms of the GNU\n" +
+				"General Public License as published by the Free Software Foundation, either version 3 of the\n" +
+				"License, or (at your option) any later version.\n" +
+				"This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;\n" +
+				"without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR\n" +
+				"PURPOSE.  See the GNU General Public License for more details.\n" +
+				"You should have received a copy of the GNU General Public License along with this program. If\n" +
+				"not, see <http://www.gnu.org/licenses/>.\n\n" +
+				"Do you accept these terms and conditions?",
+				"License information", JOptionPane.YES_NO_OPTION);
+	}
     
+    //infos
+    public static boolean isUnixOS() {
+    	return  System.getProperty("os.name").equals("Linux") ||
+    			System.getProperty("os.name").equals("Mac OS X");
+    }
+
 }
--- wftcInstaller/WftCInstaller.java
+++ wftcInstaller/WftCInstaller.java
@@ -1,34 +1,115 @@
 package wftcInstaller;
 
+import java.awt.EventQueue;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.security.CodeSource;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
-public class WftCInstaller {
+public class WftCInstaller extends JFrame {
 
-	public static void main(String[] args) {
-		try {
-			/* rename non empty dir: 
-			Files.move(new File("/home/asta666/Desktop/wtfc").toPath(), new File("/home/asta666/Desktop/wtfc2").toPath(), StandardCopyOption.ATOMIC_MOVE); */
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("xxxasd");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WftCInstaller frame = new WftCInstaller();
+					frame.setVisible(true);
+					frame.pack();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 	
-	public class Installation {
-	     String loc_path = null;
-	     String os = null;
+	public WftCInstaller() throws IOException {
+		
+		setResizable(false);
+		setTitle("War for the Colonies Installer");
+		setBounds(100, 100, 270, 220);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Installation i1 = new WftCInstaller.Installation();
+		final JFileChooser fcORG = new JFileChooser();
+		
+		JButton btnSelectMedievalIi = new JButton("Select Medieval II directory");
+		btnSelectMedievalIi.setToolTipText("Select the directory where 'Medieval II' is installed.");
+		btnSelectMedievalIi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				  fcORG.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				  fcORG.showOpenDialog(fcORG.getParent());	
+			}
+		});
+		
+		JButton btnInstallWarFor = new JButton("Install");
+		btnInstallWarFor.setToolTipText("Install 'War for the Colonies'.");
+		btnInstallWarFor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fcORG.getSelectedFile() != null) {
+					if(AuxMeth.showTextMessage() == 0) {
+						try {
+							i1.performInstall(fcORG.getSelectedFile().toString());
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "It is necessary to select the directory\nwhere"
+														+ " the game is installed first.");
+				}
+			}
+		});
+		
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(84)
+					.addComponent(btnInstallWarFor, GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE)
+					.addGap(81))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnSelectMedievalIi, GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(23)
+					.addComponent(btnSelectMedievalIi, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+					.addGap(42)
+					.addComponent(btnInstallWarFor, GroupLayout.DEFAULT_SIZE, 55, Short.MAX_VALUE)
+					.addGap(50))
+		);
+		getContentPane().setLayout(groupLayout);
+	}
+	
+	public static class Installation {
+		
 	     AuxMeth am = new AuxMeth();
 	     
-	     public void performInstall() throws IOException {
-	     //make a copy of the americas folder
-		 am.copyFolder(new File(loc_path + "/mods/americas"), new File(loc_path + "/mods/amorg" ));
-		 //run americas unpacker
-		 am.runUnpacker(loc_path, os);
+	     public void performInstall(String loc_path) throws Exception {
+	    	 File bkDir = new File(loc_path + "/wftcBK");
+	    	 CodeSource codeSource = WftCInstaller.class.getProtectionDomain().getCodeSource();
+	    	 File jarFile = new File(codeSource.getLocation().toURI().getPath());
+	    	 String jarDir = jarFile.getParentFile().getPath();
+	    	 AuxMeth.mkDirIfNotPresent(bkDir);
+	    	 File resFolder = new File(jarDir + "/wftc");
+	    	 AuxMeth.bkFilesIfExist(loc_path, resFolder, bkDir);
+	    	 AuxMeth.copyFolder(resFolder, (new File(loc_path )));
+	    	 Files.deleteIfExists((new File(loc_path + "/mods/americas/data/descr_geography_new.txt").toPath()));
+	    	 Files.deleteIfExists((new File(loc_path + "/mods/americas/data/descr_geography_new.db").toPath()));
+	    	 JOptionPane.showMessageDialog(null, "Installation complete. The backup directory is\n"
+	    			 				              + bkDir.toString());
 	    }
 	}
 }
